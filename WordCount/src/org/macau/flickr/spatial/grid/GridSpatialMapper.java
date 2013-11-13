64e9e4589617427d4cc47c9acc97f1e301fa6983
hadoop <hadoop@wangjian-desktop>
2013-11-13 10:53:39 +0800

    the grid spatial join

--- WordCount/src/org/macau/flickr/spatial/grid/GridSpatialMapper.java
+++ WordCount/src/org/macau/flickr/spatial/grid/GridSpatialMapper.java
@@ -1,49 +1,145 @@
 package org.macau.flickr.spatial.grid;
 
-import java.io.IOException;
-
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.macau.flickr.util.FlickrSimilarityUtil;
-import org.macau.flickr.util.FlickrValue;
-
 /**
+ * @author: wangjian
+ * @date: 2013-11-12
  * 
- * @author mb25428
- * Read the flickr data, extract the spatial information
- * For Example
- * The Data form:
- * ID;lat;lon;timestamp
- * 1093113743;48.89899;2.380696;973929974000
+ * the idea comes from the paper "SJMR:Parallelizing Spatial Join with MapReduce",IEEE, 2009
  * 
+ * The MapFunction is for R join S
  * The solution:
  * Partition R in one way grid and pratition S in the extension grid, which means that one grid in R just need to 
  * compare one grid in the S, but there may be too many replications in S.
  */
 
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.macau.flickr.util.FlickrSimilarityUtil;
+import org.macau.flickr.util.FlickrValue;
+import org.macau.flickr.util.spatial.ZOrderValue;
+import org.macau.flickr.spatial.partition.*;
+import org.macau.spatial.Distance;
+
+/**
+ * 
+ * @author mb25428
+ * Read the flickr data, then extract the spatial information, send the object to other machine
+ * For Example
+ * The Data form:ID;lat;lon;timestamp
+ * The Data Example:1093113743;48.89899;2.380696;973929974000
+ */
+
+
 public class GridSpatialMapper extends
 Mapper<Object, Text, IntWritable, FlickrValue>{
 
-	//get the whole universe and get the tile number,then order the tile according the hilbert value or z-order value
-	public static int tileNumber(double lat,double lon){
+	/*
+	 * use the z curve order and round-robin algorithm to find the best partition function
+	 * the universe is divided regularly into Nt tiles
+	 * each tile is number from 0 to Nt-1 according to z curve, 
+	 * and mapped to a partition p with a round robin scheme
+	 * 
+	 */
+
+	protected void setup(Context context) throws IOException, InterruptedException {
+
+		System.out.println("Grid Spatial Mapper setup");
+	}
+	
+	public static ArrayList<Integer> tileNumberOfR(double lat,double lon){
 		
-		int latNumber = (int) ((lat - FlickrSimilarityUtil.minLat)/FlickrSimilarityUtil.wholeSpaceWidth * FlickrSimilarityUtil.tilesNumber);
-		int lonNumber = (int)((lon- FlickrSimilarityUtil.minLon)/FlickrSimilarityUtil.WholeSpaceLength * FlickrSimilarityUtil.tilesNumber);
-		return latNumber + lonNumber* FlickrSimilarityUtil.tilesNumber;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int latNumber = (int) ((lat - FlickrSimilarityUtil.MIN_LAT)/FlickrSimilarityUtil.wholeSpaceWidth * FlickrSimilarityUtil.tilesNumber);
+		int lonNumber = (int)((lon- FlickrSimilarityUtil.MIN_LON)/FlickrSimilarityUtil.WholeSpaceLength * FlickrSimilarityUtil.tilesNumber);
+		list.add(ZOrderValue.parseToZOrder(latNumber, lonNumber));
+		return list;
+		
+	}
+	
+	
+	/*
+	 * the cell start from 0 to n-1
+	 * for the S set, the tile is larger than the tile of R set one threshold width
+	 */
+	public static ArrayList<Integer> tileNumberOfS(double lat, double lon){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		double tileWidth = FlickrSimilarityUtil.wholeSpaceWidth / FlickrSimilarityUtil.tilesNumber;
+		double tileHight = FlickrSimilarityUtil.WholeSpaceLength / FlickrSimilarityUtil.tilesNumber;
+		
+		int latNumber = (int) ((lat - FlickrSimilarityUtil.MIN_LAT)/FlickrSimilarityUtil.wholeSpaceWidth * FlickrSimilarityUtil.tilesNumber);
+		int lonNumber = (int)((lon- FlickrSimilarityUtil.MIN_LON)/FlickrSimilarityUtil.WholeSpaceLength * FlickrSimilarityUtil.tilesNumber);
+//		list.add(ZOrderValue.parseToZOrder(latNumber, lonNumber));
+		
+		ArrayList<Integer> latList = new ArrayList<Integer>();
+		ArrayList<Integer> lonList = new ArrayList<Integer>();
+		
+		latList.add(latNumber);
+		lonList.add(lonNumber);
+		
+		/*
+		 * get S lat tile list
+		 */
+		if( Distance.GreatCircleDistance(lat,lon,FlickrSimilarityUtil.MIN_LAT + latNumber*tileWidth,lon)< FlickrSimilarityUtil.DISTANCE_THRESHOLD ){
+			if(latNumber != 0){
+				latList.add(latNumber-1);
+			}
+		}
+		
+		if(Distance.GreatCircleDistance(lat,lon,FlickrSimilarityUtil.MIN_LAT + (latNumber+1)*tileWidth,lon) < FlickrSimilarityUtil.DISTANCE_THRESHOLD){
+			latList.add(latNumber+1);
+		}
+		
+		
+		/*
+		 * get S lon tile list
+		 */
+		if( Distance.GreatCircleDistance(lat,lon,lat,FlickrSimilarityUtil.MIN_LON + lonNumber*tileHight)< FlickrSimilarityUtil.DISTANCE_THRESHOLD ){
+			if(lonNumber != 0){
+				lonList.add(lonNumber-1);
+			}
+		}
+		
+		if(Distance.GreatCircleDistance(lat,lon,lat,FlickrSimilarityUtil.MIN_LON + (lonNumber+1)*tileHight) < FlickrSimilarityUtil.DISTANCE_THRESHOLD){
+			lonList.add(lonNumber+1);
+		}
+		
+		for(Integer la: latList){
+			for(Integer lo: lonList){
+				list.add(ZOrderValue.parseToZOrder(la, lo));
+			}
+		}
+		
+		return list;
+	}
+	
+	/*
+	 * extend the point at threshold width
+	 */
+	public static ArrayList<Integer> tileNumberList(double lat,double lon){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		
+		return list;
 		
 	}
 	
 	/**
 	 * 
 	 * @param tileNumber
-	 * @return the parition Number, I use the round-robin function
+	 * @return the partition number using the round robin algorithm
 	 */
 	public static int paritionNumber(int tileNumber){
 		
-		int number = 1;
-		return number;
+		return (tileNumber +1) % FlickrSimilarityUtil.partitionNumber;
+		
 	}
 	
 	private IntWritable outputKey = new IntWritable();
@@ -52,21 +148,65 @@ Mapper<Object, Text, IntWritable, FlickrValue>{
 	
 	public void map(Object key, Text value, Context context)
 			throws IOException, InterruptedException {
+		InputSplit inputSplit = context.getInputSplit();
+		
+		//R: 0; S:1
+		int tag;
+		
+		//get the the file name which is used for separating the different set
+		String fileName = ((FileSplit)inputSplit).getPath().getName();
+				
+		
+		
+		if(fileName.contains(FlickrSimilarityUtil.R_TAG)){
+			
+			tag = 0;
+			
+		}else{
+			tag = 1;
+		}
 		
 		long id =Long.parseLong(value.toString().split(";")[0]);
 		double lat = Double.parseDouble(value.toString().split(";")[1]);
 		double lon = Double.parseDouble(value.toString().split(";")[2]);
 		long timestamp = Long.parseLong(value.toString().split(";")[3]);
 		
+
+		ArrayList<Integer> tileList = new ArrayList<Integer>();
 		
 		outputValue.setId(id);
 		outputValue.setLat(lat);
 		outputValue.setLon(lon);
+		outputValue.setTag(tag);
 		outputValue.setTimestamp(timestamp);
 		
-		outputKey.set(tileNumber(lat,lon));
+//		System.out.println(tag);
+		if(tag == 0){
+			
+			tileList = tileNumberOfR(lat,lon);
+		}else{
+			
+			tileList = tileNumberOfS(lat,lon);
+		}
 		
-		context.write(outputKey, outputValue);
+//		System.out.println(tileList.toString().substring(1, tileList.toString().length()-1));
+		outputValue.setTiles(tileList.toString().substring(1, tileList.toString().length()-1));
+//		System.out.println(outputValue.getTiles());
+		
+		/*
+		 * for R, there is only need one tile
+		 * but for S, the data should send to other tiles
+		 */
+		for(Integer tile: tileList){
+		
+			outputValue.setTileNumber(tile);
+			outputKey.set(GridPartition.paritionNumber(tile));
+			context.write(outputKey, outputValue);
+			
+		}
 		
 	}
+	
+	
+	
 }
--- WordCount/src/org/macau/flickr/spatial/grid/GridSpatialReducer.java
+++ WordCount/src/org/macau/flickr/spatial/grid/GridSpatialReducer.java
@@ -1,7 +1,16 @@
 package org.macau.flickr.spatial.grid;
 
+/**
+ * There are two step
+ * Filter Step: 
+ * Input: the tuples belongs to the same partitions
+ * the goal is to find the paris of intersecting rectangles between the two sets by strip-based plane sweeping techniques.
+ */
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -13,44 +22,110 @@ public class GridSpatialReducer extends
 	Reducer<IntWritable, FlickrValue, Text, Text>{
 
 	private final Text text = new Text();
-	private final ArrayList<FlickrValue> records = new ArrayList<FlickrValue>();
+	private final Map<Integer,ArrayList<FlickrValue>> rMap = new HashMap<Integer,ArrayList<FlickrValue>>();
+	private final Map<Integer,ArrayList<FlickrValue>> sMap = new HashMap<Integer,ArrayList<FlickrValue>>();
+
 	
 	public void reduce(IntWritable key, Iterable<FlickrValue> values,
 			Context context) throws IOException, InterruptedException{
+		
+		int count = 0;
+		System.out.println(key);
+//		System.out.println(values.toString());
+		
 		for(FlickrValue value:values){
-			FlickrValue recCopy = new FlickrValue(value);
-		    records.add(recCopy);
+//			System.out.println(value);
+//			System.out.println("tile " + value.getTileNumber());
+//			System.out.println(count++);
+			/*We need new a FlickrValue, if not, the values in Map.getTag() will become the same
+			 * this may because the address of the value is the same, when change a value, all the values
+			 * in the Map.getTag will become the same
+			 * 
+			 * */
+			FlickrValue fv = new FlickrValue(value);
+			
+			//R
+			if(fv.getTag() == 0){
+				if(rMap.containsKey(value.getTileNumber())){
+					
+					rMap.get(value.getTileNumber()).add(fv);
+
+				}else{
+
+					ArrayList<FlickrValue> recordList = new ArrayList<FlickrValue>();
+					recordList.add(fv);
+					rMap.put(new Integer(value.getTileNumber()),recordList);
+					
+				}
+			}else{
+				if(sMap.containsKey(value.getTileNumber())){
+					
+					sMap.get(value.getTileNumber()).add(fv);
+
+				}else{
+
+					ArrayList<FlickrValue> recordList = new ArrayList<FlickrValue>();
+					recordList.add(fv);
+					sMap.put(new Integer(value.getTileNumber()),recordList);
+					
+				}
+			}
 		}
 		
-		//System.out.println("key is " + key + "record size is " + records.size());
+
 		
-		for (int i = 0; i < records.size(); i++) {
-			FlickrValue rec1 = records.get(i);
-		    for (int j = i + 1; j < records.size(); j++) {
-		    	FlickrValue rec2 = records.get(j);
-/*		    	System.out.println(rec1);
-		    	System.out.println(rec2);*/
-//		    	if(FlickrSimilarityUtil.TemporalSimilarity(rec1, rec2)){
-//		    		continue;
-//		    	}
-		    	
-		    	if(FlickrSimilarityUtil.SpatialSimilarity(rec1, rec2)){
-		    		long ridA = rec1.getId();
+		Iterator it = rMap.entrySet().iterator();
+		
+		
+		while(it.hasNext()){
+			Map.Entry<Integer,ArrayList<FlickrValue>> m = (Map.Entry<Integer,ArrayList<FlickrValue>>)it.next();
+			m.getKey();
+			ArrayList<FlickrValue> rRecords = m.getValue();
+			//System.out.println(sMap.get(m.getKey()));
+			ArrayList<FlickrValue> sRecords = new ArrayList<FlickrValue>();
+			
+			/*
+			 * This is important,because the sMap may don't have the key value
+			 * 
+			 */
+			if(sMap.get(m.getKey()) != null){
+				sRecords = sMap.get(m.getKey());
+			}
+			
+			
+			System.out.println(rRecords.size() + " r" );
+			System.out.println(sRecords.size() + " s" );
+			
+			
+//			brute force
+			for (int i = 0; i < rRecords.size(); i++) {
+				FlickrValue rec1 = rRecords.get(i);
+				
+				//System.out.println(rec1);
+			    for (int j = 0; j < sRecords.size(); j++) {
+			    	
+			    	
+			    	FlickrValue rec2 = sRecords.get(j);
+			    	//System.out.println(rec2);
+			    	
+			    	long ridA = rec1.getId();
 		            long ridB = rec2.getId();
-		            if (ridA < ridB) {
-		                long rid = ridA;
-		                ridA = ridB;
-		                ridB = rid;
-		            }
-		            
-		            System.out.println(ridA + "%" + ridB);
-		            text.set(ridA + "%" + ridB);
-		            context.write(text, new Text(""));
-		    	}
-		    	
-		    }
+			    	if(FlickrSimilarityUtil.SpatialSimilarity(rec1, rec2)){
+			    		
+//			            if (ridA < ridB) {
+//			                long rid = ridA;
+//			                ridA = ridB;
+//			                ridB = rid;
+//			            }
+
+			            text.set(ridA + "%" + ridB);
+			            context.write(text, new Text(""));
+			    	}
+			    	
+			    }
+			}
 		}
-		records.clear();
+		
 	}
 	
 }
