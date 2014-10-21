b62c65cbb44c5c60aec6d63874a3defb6ac618e6
mb25428 <mb25428@FST-259QNLQONGN>
2014-10-21 15:17:31 +0800

    add some local package file

---
+++ WordCount/src/org/macau/local/sample/RangeQuality.java
@@ -0,0 +1,16 @@
+package org.macau.local.sample;
+
+public class RangeQuality {
+
+	public static void main(String[] args){
+		System.out.println("The range Quality");
+		//read the R Distribution
+		
+		//read the S Distribution
+		
+		//calculate the distribution of the 
+		
+		
+		//find the best way to get the result.
+	}
+}
