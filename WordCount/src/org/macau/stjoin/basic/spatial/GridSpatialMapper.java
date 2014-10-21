808d6395ce88b264c16ad8b7d262fa8c789da0d4
hadoop <hadoop@wangjian-desktop>
2014-10-21 16:39:34 +0800

    change one line

--- WordCount/src/org/macau/stjoin/basic/spatial/GridSpatialMapper.java
+++ WordCount/src/org/macau/stjoin/basic/spatial/GridSpatialMapper.java
@@ -3,6 +3,7 @@ package org.macau.stjoin.basic.spatial;
 /**
  * @author: wangjian
  * @date: 2013-11-12
+ * Last modify date: 2014-10-21
  * 
  * 
  * the idea comes from the paper "SJMR:Parallelizing Spatial Join with MapReduce",IEEE, 2009
@@ -40,7 +41,6 @@ import org.macau.spatial.Distance;
  * For Example
  * The Data form:ID;locationID, lat;lon; timestamp, textual 
  * The Data Example:1093113743:215929:48.89899:2.380696:973929974000:0;82;525;2479;19649;25431;31250;51203
- * 
  * 
  * 
  */
