fb3c9eccd88e973b5fa26db780f4fe87f03a8bad
hadoop <hadoop@wangjian-desktop>
2014-12-29 17:41:46 +0800

    group join

---
+++ WordCount/src/org/macau/stjoin/group/GroupJoinJob.java
@@ -0,0 +1,54 @@
+package org.macau.stjoin.group;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.macau.flickr.job.TemporalSimilarityJoin;
+import org.macau.flickr.util.FlickrSimilarityUtil;
+import org.macau.flickr.util.FlickrValue;
+
+
+/**
+ * 
+ * @author hadoop
+ * Map: Input:  KEY  : 
+ *              Value:
+ *      output: KEY  :
+ *      		Value:
+ *      
+ * Reduce: Input: KEY  :
+ * 				  Value:
+ */
+public class GroupJoinJob {
+
+	public static boolean TemporalSimilarityBasicJoin(Configuration conf) throws Exception{
+		
+		Job basicJob = new Job(conf,"Temporal Basic Similarity Join");
+		basicJob.setJarByClass(TemporalSimilarityJoin.class);
+		
+		basicJob.setMapperClass(GroupJoinMapper.class);
+//		basicJob.setCombinerClass(TemporalJoinReducer.class);
+		
+//		basicJob.setReducerClass(GroupJoinReducer.class);
+		
+		basicJob.setMapOutputKeyClass(Text.class);
+		basicJob.setMapOutputValueClass(IntWritable.class);
+		
+//		basicJob.setOutputKeyClass(Text.class);
+//		basicJob.setOutputValueClass(Text.class);
+		basicJob.setNumReduceTasks(6);
+		
+		FileInputFormat.addInputPath(basicJob, new Path(FlickrSimilarityUtil.flickrInputPath));
+		FileOutputFormat.setOutputPath(basicJob, new Path(FlickrSimilarityUtil.flickrOutputPath));
+		
+		if(basicJob.waitForCompletion(true))
+			return true;
+		else
+			return false;
+	}
+}
