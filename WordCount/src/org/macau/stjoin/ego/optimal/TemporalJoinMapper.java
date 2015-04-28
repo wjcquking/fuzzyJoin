f6e1b74af2ff7ee04d5077a63cd0c90e73d720f3
wj <wjcqu@hotmail.com>
2015-04-28 23:59:18 +0800

    Optimal temporal Join Job

---
+++ WordCount/src/org/macau/stjoin/ego/optimal/TemporalJoinMapper.java
@@ -0,0 +1,116 @@
+package org.macau.stjoin.ego.optimal;
+
+/**
+ * The Mapper uses the temporal information
+ * R send same number 
+ * S send one more time interval 
+ * 
+ */
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.macau.flickr.util.FlickrSimilarityUtil;
+import org.macau.flickr.util.FlickrValue;
+
+public class TemporalJoinMapper extends
+	Mapper<Object, Text, LongWritable, FlickrValue>{
+	
+	private final LongWritable outputKey = new LongWritable();
+	
+	private final FlickrValue outputValue = new FlickrValue();
+	
+	protected void setup(Context context) throws IOException, InterruptedException {
+
+		System.out.println("Temporal mapper Start at " + System.currentTimeMillis());
+	}
+	
+	public static String convertDateToString(Date date){
+		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd");
+		return df.format(date);
+	}
+	
+	public static Date convertLongToDate(Long date){
+		return new Date(date);
+	}
+	
+	
+	public void map(Object key, Text value, Context context)
+			throws IOException, InterruptedException {
+		
+		InputSplit inputSplit = context.getInputSplit();
+				
+		//get the the file name which is used for separating the different set
+		String fileName = ((FileSplit)inputSplit).getPath().getName();
+				
+		
+		int tag = FlickrSimilarityUtil.getTagByFileName(fileName);
+		
+		long id =Long.parseLong(value.toString().split(":")[0]);
+		double lat = Double.parseDouble(value.toString().split(":")[2]);
+		double lon = Double.parseDouble(value.toString().split(":")[3]);
+		long timestamp = Long.parseLong(value.toString().split(":")[4]);
+		
+		
+		long timeInterval = timestamp / FlickrSimilarityUtil.TEMPORAL_THRESHOLD;
+		
+//		outputValue = new FlickrValue(FlickrSimilarityUtil.getFlickrVallueFromString(value.toString()));
+		
+		outputValue.setTileNumber((int)timeInterval);
+		
+		outputValue.setId(id);
+		outputValue.setLat(lat);
+		outputValue.setLon(lon);
+		outputValue.setTag(tag);
+		
+		//the textual information
+		outputValue.setTiles(value.toString().split(":")[5]);
+		
+//		System.out.println(value.toString().split(":")[5]);
+		
+		outputValue.setTimestamp(timestamp);
+		
+		
+//		if(tag == FlickrSimilarityUtil.S_tag && timeInterval % 10 == 0){
+//			
+//			outputKey.set(timeInterval/10 - 1);
+//			context.write(outputKey, outputValue);
+//			
+//		}
+//		
+//		outputKey.set(timeInterval/10);
+//		context.write(outputKey, outputValue);
+//		
+		//The Original temporal partition, for each time interval, it is a partition, for the R
+		//the time interval is the key, while for the S set, it should set to three time interval
+		if(tag == FlickrSimilarityUtil.S_tag){
+			
+			for(int i = -1; i<=1;i++){
+				outputKey.set(timeInterval + i);
+				outputValue.setTileNumber((int)timeInterval + i);
+				context.write(outputKey, outputValue);
+			}
+			
+		}else{
+			
+			//for the R set
+			outputKey.set(timeInterval);
+			context.write(outputKey, outputValue);
+		}
+		
+	
+		
+		
+		
+		
+	}
+	protected void cleanup(Context context) throws IOException, InterruptedException {
+		System.out.println("The Temporal mapper end at " + System.currentTimeMillis() + "\n");
+	}
+}
---
+++ WordCount/src/org/macau/stjoin/ego/optimal/TemporalJoinReducer.java
@@ -0,0 +1,290 @@
+package org.macau.stjoin.ego.optimal;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.macau.flickr.util.FlickrSimilarityUtil;
+import org.macau.flickr.util.FlickrValue;
+
+import org.macau.stjoin.basic.temporal.TemporalComparator;;
+
+
+public class TemporalJoinReducer extends
+	Reducer<LongWritable, FlickrValue, Text, Text>{
+		
+		
+		private final Text text = new Text();
+		
+		private final Map<Integer,ArrayList<FlickrValue>> rMap = new HashMap<Integer,ArrayList<FlickrValue>>();
+		private final Map<Integer,ArrayList<FlickrValue>> sMap = new HashMap<Integer,ArrayList<FlickrValue>>();
+		
+		private final List<Long> rCount = new ArrayList<Long>();
+		private final List<Long> sCount = new ArrayList<Long>();
+		private final List<Long> wCount = new ArrayList<Long>();
+		
+		private long wCompareCount = 0;
+		private long tCompareCount = 0;
+		private long sCompareCount = 0;
+		private long oCompareCount = 0;
+
+		protected void setup(Context context) throws IOException, InterruptedException {
+
+			System.out.println("Temporal reducer Start at " + System.currentTimeMillis());
+		}
+		
+		public void reduce(LongWritable key, Iterable<FlickrValue> values,
+				Context context) throws IOException, InterruptedException{
+			
+			
+			for(FlickrValue value:values){
+				
+				FlickrValue fv = new FlickrValue(value);
+				
+			    if(fv.getTag() == FlickrSimilarityUtil.R_tag){
+			    	
+			    	if(rMap.containsKey(value.getTileNumber())){
+				    	
+				    	rMap.get(value.getTileNumber()).add(new FlickrValue(fv));
+				    	
+				    }else{
+				    	
+				    	ArrayList<FlickrValue> list = new ArrayList<FlickrValue>();
+				    	
+				    	list.add(fv);
+				    	
+				    	rMap.put(value.getTileNumber(), list);
+				    	
+				    }
+			    }else{
+			    	
+			    	if(sMap.containsKey(value.getTileNumber())){
+				    	
+				    	sMap.get(value.getTileNumber()).add(new FlickrValue(fv));
+				    	
+				    }else{
+				    	
+				    	ArrayList<FlickrValue> list = new ArrayList<FlickrValue>();
+				    	
+				    	list.add(fv);
+				    	
+				    	sMap.put(value.getTileNumber(), list);
+				    }
+			    }
+			    
+			    
+			}
+			
+		
+			/*****************************
+			 * 
+			 * 
+			 * Sort the record in the Map to solve the compare time
+			 * For example
+			 * One record in one time interval, it just need compare record before the same position in the adjacent 
+			 * time interval
+			 * 
+			 * Notice: Now this is no use in the following function but I think it is useful
+			 * 
+			 ****************************/
+			long rSetSize = 0;
+			long sSetSize = 0;
+			long  wholeSize = 0;
+			// Sort the List in the Map
+			for(java.util.Iterator<Integer> i = rMap.keySet().iterator();i.hasNext();){
+				
+				TemporalComparator comp = new TemporalComparator();
+				int obj = i.next();
+				Collections.sort(rMap.get(obj),comp);
+				rSetSize += rMap.get(obj).size();
+				
+			}
+			
+			for(java.util.Iterator<Integer> i = sMap.keySet().iterator();i.hasNext();){
+				
+				TemporalComparator comp = new TemporalComparator();
+				int obj = i.next();
+				Collections.sort(sMap.get(obj),comp);
+				sSetSize += sMap.get(obj).size();
+				
+			}
+			wholeSize = sSetSize + rSetSize;
+			rCount.add(rSetSize);
+			sCount.add(sSetSize);
+			wCount.add(wholeSize);
+			
+
+			
+			for(java.util.Iterator<Integer> obj = rMap.keySet().iterator();obj.hasNext();){
+				
+				Integer i = obj.next();
+				
+				
+				if(sMap.containsKey(i)){
+					
+					
+					for(int j = 0;j < rMap.get(i).size();j++){
+						
+						FlickrValue value1 = rMap.get(i).get(j);
+						
+						//for the same tail, there is no need for comparing
+						
+						for(int k = 0; k < sMap.get(i).size();k++){
+							FlickrValue value2 = sMap.get(i).get(k);
+							tCompareCount++;
+							if(FlickrSimilarityUtil.TemporalSimilarity(value1, value2)){
+								sCompareCount++;
+								if(FlickrSimilarityUtil.SpatialSimilarity(value1, value2)){
+									
+									oCompareCount++;
+									if(FlickrSimilarityUtil.TextualSimilarity(value1, value2)){
+										
+										
+										long ridA = value1.getId();
+							            long ridB = value2.getId();
+							            if (ridA < ridB) {
+							                long rid = ridA;
+							                ridA = ridB;
+							                ridB = rid;
+							            }
+									
+						            
+						            
+							            text.set("" + ridA + "%" + ridB);
+							            context.write(text, new Text(""));
+									}
+								}
+							}
+						}
+						
+//						// for the adjacent tail
+//						if(sMap.containsKey(i+1)){
+//							
+//							for(int m = 0; m < sMap.get(i+1).size();m++){
+//								
+//								FlickrValue value3 = sMap.get(i+1).get(m);
+//								
+//								tCompareCount++;
+//								if(FlickrSimilarityUtil.TemporalSimilarity(value1, value3)){
+//									
+////									if(FlickrSimilarityUtil.SpatialSimilarity(value1, value3) && FlickrSimilarityUtil.TextualSimilarity(value1, value3)){
+//									sCompareCount++;
+//									if(FlickrSimilarityUtil.SpatialSimilarity(value1, value3)){
+//										
+//										oCompareCount++;
+//										if(FlickrSimilarityUtil.TextualSimilarity(value1, value3)){
+//										
+//											long ridA = value1.getId();
+//								            long ridB = value3.getId();
+//								            
+//								            if (ridA < ridB) {
+//								                long rid = ridA;
+//								                ridA = ridB;
+//								                ridB = rid;
+//								            }
+//								            
+//											text.set("" + ridA + "%" + ridB);
+//								            context.write(text, new Text(""));
+//										}
+//									}
+//								}else{
+//									break;
+//								}
+//								
+//							}
+//						}
+						
+					}
+				}
+			}
+
+			
+			rMap.clear();
+			sMap.clear();
+		}
+		
+		/*
+		 * (non-Javadoc)
+		 * @see org.apache.hadoop.mapreduce.Mapper#cleanup(org.apache.hadoop.mapreduce.Mapper.Context)
+		 */
+		protected void cleanup(Context context) throws IOException, InterruptedException {
+			System.out.println("clean up");
+			System.out.println("The Reducer End at"+System.currentTimeMillis());
+			long rMax = 0;
+			long rMin = 1000000;
+			long rC =0;
+			System.out.println("R data set");
+			for(long i : rCount){
+//				System.out.println(i);
+				if(i > rMax){
+					rMax = i;
+				}
+				if(i < rMin){
+					rMin = i;
+				}
+				rC += i;
+			}
+			
+			System.out.println("r Max " + rMax);
+			System.out.println("r Min " + rMin);
+			System.out.println("r Count" + rC);
+			
+			
+			long sMax = 0;
+			long sMin = 1000000;
+			long sC =0;
+			System.out.println("S data set");
+			
+			for(long i : sCount){
+//				System.out.println(i);
+				if(i > sMax){
+					sMax = i;
+				}
+				if(i < sMin){
+					sMin = i;
+				}
+				sC += i;
+			}
+			
+			System.out.println();
+			System.out.println("s Max " + sMax);
+			System.out.println("s Min " + sMin);
+			System.out.println("s Count" + sC);
+			
+
+			long wMax = 0;
+			long wMin = 1000000;
+			long wC =0;
+			for(long i : wCount){
+				if(i > wMax){
+					wMax = i;
+				}
+				if(i < wMin){
+					wMin = i;
+				}
+				wC += i;
+			}
+			
+			System.out.println("w Max " + wMax);
+			System.out.println("w Min " + wMin);
+			System.out.println("w Count" + wC);
+			
+			
+			System.out.println("T compare Count " + tCompareCount);
+			System.out.println("S compare Count " + sCompareCount);
+			System.out.println("Textual Compare Count " + oCompareCount);
+			wCompareCount = tCompareCount+ sCompareCount + oCompareCount;
+			System.out.println("The total Count " + wCompareCount);
+			
+			
+		}
+	}
+
